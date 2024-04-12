fd77b8c5de1d8792faed9f62e9aa7ca9f8d3678a
Luca10992 <lucalillo.99.1@gmail.com>
2024-04-12 16:48:11 +0200

    feat. complete milestone 4

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::select(['id', 'type_id', 'title', 'description', 'thumb'])->with(['type:id,label,color', 'technologies:id,label'])->paginate(16);
+        return response()->json($projects);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+}
